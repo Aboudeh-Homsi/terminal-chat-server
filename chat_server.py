10d3297ce8466b816af7465fbc3a8755bb0a9c1f
Aboudeh-Homsi <66535711+Aboudeh-Homsi@users.noreply.github.com>
2025-06-28 10:53:02 +0000

    Refactor and finalize WebSocket chat server implementation

--- chat_server.py
+++ chat_server.py
@@ -5,7 +5,11 @@ app = FastAPI()
 clients = []
 
 @app.websocket("/ws")
-async def websocket_et
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    clients.append(websocket)
+    try:
+        while True:
             data = await websocket.receive_text()
             for client in clients:
                 if client != websocket:
